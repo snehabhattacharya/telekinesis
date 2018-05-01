389ed3d8aaf39fffa81f1886b6e10414efe7daef
snehabhattacharya <sneha.22.7@gmail.com>
2018-05-01 12:32:12 -0400

    dataloader

---
+++ cnn.py
@@ -0,0 +1,140 @@
+import numpy as np 
+import torch
+import torch.nn as nn
+from torch.autograd import Variable
+import matplotlib.pyplot as plt
+import torch.nn.functional as F
+import torch.optim as optim
+from dataloader import EEGDataset, my_collate, my_cnn_collate
+from torch.utils.data import DataLoader
+
+
+class CNN_net(nn.Module):
+    def __init__(self, 
+                 in_chans,
+                 n_classes,
+                 batch_size,
+                 input_time_length,
+                 kernel_size,
+                 #final_conv_length,
+                 n_filters_time=16,
+                 n_filters_spat=10,
+                 
+                 filter_time_length=10,
+                 pool_time_length=3,
+                 pool_time_stride=3,
+                 n_filters_2=50,
+                 filter_length_2=10,
+                 n_filters_3=100,
+                 filter_length_3=10,
+                 n_filters_4=200,
+                 filter_length_4=10,
+                 #first_nonlin=elu,
+                 first_pool_mode='max',
+                 #first_pool_nonlin=identity,
+                 #later_nonlin=elu,
+                 later_pool_mode='max',
+                 #later_pool_nonlin=identity,
+                 drop_prob=0.5,
+                 double_time_convs=False,
+                 split_first_layer=True,
+                 batch_norm=True,
+                 batch_norm_alpha=0.1,
+                 stride_before_pool=False):
+        super(CNN_net, self).__init__()
+        self.in_chans = in_chans
+        self.kernel_size = kernel_size
+        self.batch_size = batch_size
+        self.n_filters_time = n_filters_time
+        self.input_time_length = input_time_length
+        self.n_filters_spat = n_filters_spat
+        self.n_classes = n_classes
+        self.time_conv = nn.Sequential(
+            nn.Conv2d(self.in_chans,self.n_filters_time, kernel_size=2,
+            stride=2),
+            #nn.ReLU(),
+            nn.MaxPool2d(2))
+        self.spat_conv = nn.Sequential(
+            nn.Conv2d(self.n_filters_time, self.n_filters_spat,
+                     (1, 1),
+                     stride=(1, 1),),
+            nn.BatchNorm2d(self.n_filters_spat),
+            nn.ReLU())
+
+        self.fc = nn.Linear(self.n_filters_time, 
+                       self.n_classes)
+
+    def forward(self, x):
+        x = x.float()
+        N = x.size()[0]
+        x = x[:19680]
+        #x = x.view()
+        #x = torch.to_numpy()
+        x = x.view(1230, 64,4,4)
+        #print x.size(), "new"
+        x = self.time_conv(x)
+        #print x.size(), "after time Conv2d"
+        # x = self.spat_conv(x)
+        x = x.view(x.size(0),-1)
+        
+        x = self.fc(x)
+        #x = F.softmax
+        return x
+# torch.optim.Adam
+model = CNN_net(in_chans=64, n_classes=3, input_time_length=1, batch_size=1, kernel_size=2)
+optimizer = torch.optim.Adam(model.parameters(),lr=0.01)
+criterion = nn.CrossEntropyLoss()
+fpath = "/home/sneha/UMASS/UBICOMP/project/data/pre_processed/"
+dset = EEGDataset(fpath)
+loader = DataLoader(dset,num_workers=2, batch_size=1, collate_fn=my_cnn_collate)
+# for data, target in loader:
+    
+num_epochs = 10
+# criterion = nn.CrossEntropyLoss()
+# # optimizer = torch.optim.Adam(
+# #     model.parameters(), lr=learning_rate, weight_decay=1e-5)
+for epoch in range(num_epochs):
+    for data, target in loader:
+        data = Variable(data)
+        #target = Variable(target)
+        output = model(data)
+        _, preds = torch.max(output.data,1)
+        #print output.size(), "output", preds.size(), "preds"
+        target = target[:19680]
+        new_target = torch.zeros(1230)
+        for i in range(0,len(target),16):
+            if not i > 1230:
+                new_target[i] = target[i]
+        # print new_target.size(), "new_target"
+        # print preds.size()
+        loss = criterion(output, Variable(new_target).long())
+        # ===================backward====================
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        #print loss
+    # ===================log========================
+    print'epoch [{}/{}], loss:{:.4f}'.format(epoch + 1, num_epochs, loss.data[0])
+
+
+
+    # def optimse(self,X,y,lr, num_batches):
+    #     optimizer = optim.Adam(self.parameters(),lr=lr)
+    #     criterion = nn.CrossEntropyLoss()
+    #     # optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+    #     X =X
+    #     for epoch in range(1):
+    #         for n in range(num_batches):
+    #             X_batch = None
+    #             y_batch = None
+    #             indices = np.random.choice(np.arange(X.shape[0]), 200)
+    #             X_batch = X[indices]
+    #             y_batch = y[indices]
+    #             print len(X_batch), "size"
+    #             optimizer.zero_grad()
+    #             output = self.forward(X_batch)
+    #             y_batch = Variable(torch.from_numpy(y_batch)).long()
+    #             loss = criterion(output, y_batch)
+    #             optimizer.step()
+    #             # print loss
+    
---
+++ data/dataloader.py
@@ -0,0 +1,46 @@
+import torch
+import numpy as np
+from torch.utils.data import Dataset, DataLoader, ConcatDataset
+import os
+
+def my_collate(batch):
+    #data = torch.stack([torch.from_numpy(b) for b in batch], 0)
+    data = [torch.from_numpy(item[:,:64]) for item in batch]
+    # target = [torch.from_numpy(item[:,65]) for item in batch]
+    #data = torch.stack(data,0)
+    # print data.shape
+    return torch.cat(data, 0)
+
+def my_cnn_collate(batch):
+    data = [torch.from_numpy(item[:,:64]) for item in batch]
+    target = [torch.from_numpy(item[:,64]) for item in batch]
+    #target = torch.LongTensor(target)
+    return [torch.cat(data,0), torch.cat(target,0)]
+    
+
+
+
+class EEGDataset(Dataset):
+    def __init__(self, data_dir):
+        self.data_files = os.listdir(data_dir)
+        #print len(self.data_files), "before"
+        self.new_data_files = []
+        for d in self.data_files:
+            if not "_2.npy" in d and not "_1.npy" in d:
+                self.new_data_files.append(d)
+
+        self.data_dir = data_dir
+
+    def __getindex__(self, idx):
+        return load_file(self.new_data_files[idx])
+
+    def __len__(self):
+        return len(self.new_data_files)
+    
+    def __getitem__(self, idx):
+        sample = np.load(self.data_dir + self.new_data_files[idx])
+        #print sample.shape, "sample"
+        return sample
+
+
+
